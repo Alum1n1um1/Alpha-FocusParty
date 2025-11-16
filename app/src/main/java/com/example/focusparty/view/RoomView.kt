20548859e12616133da215b9812201ae24550144
Alum1n1um1 <celian.bellenge@gmail.com>
2025-11-15 22:20:15 -0500

    Merge2

--- app/src/main/java/com/example/focusparty/view/RoomView.kt
+++ app/src/main/java/com/example/focusparty/view/RoomView.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,6 +13,16 @@ import androidx.navigation.NavHostController
 import com.example.focusparty.model.Jalon
 import com.example.focusparty.model.Room
 import com.example.focusparty.viewmodel.RoomViewModel
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.icons.filled.Pause
 
 @Composable
 fun RoomScreen(
@@ -86,5 +95,90 @@ fun JalonItem(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(text=jalon.name+"/"+jalon.id)
+    }
+}
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+@Composable
+fun RoomScreen(vm: RoomViewModel, navController: NavHostController)
+{
+    Column(Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.background))
+    {
+        SalonTopBar(vm)
+        Text("Todo")
+    }
+}
+
+@Composable
+fun SalonTopBar(vm: RoomViewModel) {
+
+    Surface()
+    {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .windowInsetsPadding(WindowInsets.statusBars)
+                .background(MaterialTheme.colorScheme.primary),
+            verticalAlignment = Alignment.CenterVertically
+        )
+        {
+
+            // --- Zone gauche ---
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.CenterStart
+            )
+            {
+                Text("Nom du salon")
+            }
+
+            // --- Zone centrale ---
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            )
+            {
+                Row()
+                {
+                    Text("Statut du salon")
+                    IconButton(
+                        {},
+                        colors = IconButtonDefaults.iconButtonColors(
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        )
+                    )
+                    {
+                        Icon(Icons.Default.Pause, "Changer le statut")
+                    }
+                }
+            }
+
+            // --- Zone droite ---
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.CenterEnd
+            )
+            {
+                Text("Personnes connectées")
+            }
+        }
     }
 }
